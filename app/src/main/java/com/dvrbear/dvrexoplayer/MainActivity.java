6531f24962d47e73b4d37a8e0a6930da5d2fa39c
dvrbear <dvrbear@mail.ru>
2016-11-28 20:48:44 +0200

    test111

--- app/src/main/java/com/dvrbear/dvrexoplayer/MainActivity.java
+++ app/src/main/java/com/dvrbear/dvrexoplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int test = 111;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
